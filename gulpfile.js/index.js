0717d34268820d7f162f174189b6ff9a6d1c98c4
Paris Kostopoulos <pariskwsto@gmail.com>
2023-01-26 22:57:12 +0200

    feat(gulp): add gulp icon sprites and fix styles

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,7 +1,9 @@
 const { build } = require("./tasks/build");
+const { icons } = require("./tasks/sprites");
 const { styles } = require("./tasks/styles");
 const { watch } = require("./tasks/watch");
 
 exports.build = build;
+exports.icons = icons;
 exports.styles = styles;
 exports.watch = watch;
