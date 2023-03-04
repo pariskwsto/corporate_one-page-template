4cde65a90a2cd05888af0532495416b677090412
Paris Kostopoulos <pariskwsto@gmail.com>
2023-03-04 14:00:40 +0200

    feat(reveal-on-scroll): add reveal on scroll for services and testimonials

--- src/assets/js/App.js
+++ src/assets/js/App.js
@@ -1,3 +1,9 @@
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
+
+const serviceItemEls = document.querySelectorAll(".service-item");
+const testimonialEls = document.querySelectorAll(".testimonial");
 
 new MobileMenu();
+new RevealOnScroll(serviceItemEls, 0.7);
+new RevealOnScroll(testimonialEls, 0.6);
